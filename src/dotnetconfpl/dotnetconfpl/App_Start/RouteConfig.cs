376384703e83a65cf9866f28fe870c00224194d7
michal-franc <michal.franc@live.com>
2013-06-06 21:03:53 +0200

    changes in layout page

--- src/dotnetconfpl/dotnetconfpl/App_Start/RouteConfig.cs
+++ src/dotnetconfpl/dotnetconfpl/App_Start/RouteConfig.cs
@@ -9,16 +9,10 @@ namespace dotnetconfpl
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            //routes.MapRoute(
-            //    name: "Default",
-            //    url: "{controller}/{action}/{id}",
-            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            //);
-
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "CountDown", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
--- src/dotnetconfpl/dotnetconfpl/Controllers/HomeController.cs
+++ src/dotnetconfpl/dotnetconfpl/Controllers/HomeController.cs
@@ -4,13 +4,9 @@ namespace dotnetconfpl.Controllers
 {
     public class HomeController : Controller
     {
-        //
-        // GET: /Home/
-
         public ActionResult Index()
         {
             return View();
         }
-
     }
 }
