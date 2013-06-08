52a73b0c85875a9ebe4438b28fb5669b81df7314
michal-franc <michal.franc@live.com>
2013-06-08 10:24:52 +0200

    little refactoring in deployment/run script

--- src/dotnetconfpl/deploy/run.ps1
+++ src/dotnetconfpl/deploy/run.ps1
@@ -1,3 +1,8 @@
 Set-ExecutionPolicy Bypass
 Import-Module .\Scripts\psake.psm1
-Invoke-psake .\deploy.ps1 -parameters @{"userName"="$($args[0])";"password"="$($args[1])";"ftpAddress"="$($args[2])"}
+
+$userName = $($args[0]);
+$password = $($args[1]);
+$ftpAddress = $($args[2]);
+
+Invoke-psake .\deploy.ps1 -parameters @{"userName"="$userName";"password"="$password";"ftpAddress"="$ftpAddress"}
