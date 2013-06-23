f5b5785cf5480fad703455519613e52c8ea4ce24
jj09 <jakub.jedryszek@gmail.com>
2013-06-23 16:51:52 -0500

    Add about page

--- src/dotnetconfpl/dotnetconfpl/Controllers/HomeController.cs
+++ src/dotnetconfpl/dotnetconfpl/Controllers/HomeController.cs
@@ -18,5 +18,11 @@ namespace dotnetconfpl.Controllers
         {
             return View();
         }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
     }
 }
