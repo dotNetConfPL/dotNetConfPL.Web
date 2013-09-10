89773a2b90df7a7face10d56c02882f6bf031dd6
LaMik <lam.michal.franc@gmail.com>
2013-09-10 21:06:09 +0200

    - deleted home controller from routing

--- src/dotnetconfpl/dotnetconfpl/App_Start/RouteConfig.cs
+++ src/dotnetconfpl/dotnetconfpl/App_Start/RouteConfig.cs
@@ -9,6 +9,8 @@ namespace dotnetconfpl
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapRoute("HomeRoute", "{action}/{id}", new { controller = "Home", action = "Index", id = UrlParameter.Optional });
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
