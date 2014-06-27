21e1a31d9cf3ccefc235332a2684597dfeb1e931
jj09 <jakub.jedryszek@gmail.com>
2014-06-26 23:26:11 -0500

    Add hardcoded JSON for Mobile App testing

--- src/dotnetconfpl/dotnetconfpl/Controllers/HomeController.cs
+++ src/dotnetconfpl/dotnetconfpl/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 using RestSharp;
 using System.Web;
+using System.Collections.Generic;
 
 namespace dotnetconfpl.Controllers
 {
@@ -138,5 +139,61 @@ namespace dotnetconfpl.Controllers
 
             return View("~/Views/Home/CheckStream.cshtml");
         }
+
+        public ActionResult Data()
+        {
+            var sessions = new List<Session>();
+
+            var speaker1 = new Speaker 
+            {
+                Name = "Maciej Aniserowicz",
+                About = "MVP i najpopularniejszy blogger w polskim środowisku .NET. Miłośnik testów jednostkowych, Gita i Resharpera, zatwardziały wróg TFSa i Sharepointa. Z zawodu programista/architekt .NET w firmie Ultrico. Wcześniej pracował w firmie Predica. Przez pewien czas był także Freelancerem. Ponadto jest dwukrotnym finalistą ImagineCup i byłym redaktorem CodeGuru.pl.",
+                Photo = "http://dotnetconfpl.apphb.com/Content/images/maciej_aniserowicz.jpg"
+            };
+
+            var speaker2 = new Speaker
+            {
+                Name = "Maciej Grabek",
+                About = "MVP, blogger, głównie zajmuje się platformą Windows Phone. Prelegent na Microsoft Technology Summit w 2011 i 2012 roku, redaktor CodeGuru.pl, współtwórca serii webcastów poświęconych technologii Windows Phone 7 oraz Windows Phone 8. Ponadto współzałożyciel Toruńskiej Grupy Developerów .NET. Zajmuje się .NETem od czasów studiów i wersji 1.1.",
+                Photo = "http://dotnetconfpl.apphb.com/Content/images/maciej_grabek.jpg"
+            };
+
+            var session1 = new Session
+            {
+                Time = "15:00 - 16:00",
+                Title = "Testy jednostkowe w .NET",
+                Speaker = speaker1,
+                About = "Podczas sesji zaprezentowane zostanie pragmatyczne podejście do pisania testów jednostkowych w .NET. Teoria w połączeniu z praktyką zademonstrują dlaczego automatyczne testowanie oprogramowania jest niezmiernie ważne oraz... jak robić to bezboleśnie."
+            };
+
+            var session2 = new Session
+            {
+                Time = "16:00 - 17:00",
+                Title = "Windows Phone 8 Tips & Tricks",
+                Speaker = speaker2,
+                About = "Windows Phone to dynamicznie rozwijająca się platforma, która zdobywa coraz większą rzeszę zwolenników i programisów. Okazuje się, że posiada również pewne arkana, o których nie wszyscy wiedzą, a które pozwalają tworzyć lepsze i bardziej przyjazne użytkownikom aplikacje. Dołącz się do konreferncji i przekonaj co jeszcze warto wiedzieć o Windows Phone."
+            };
+
+            sessions.Add(session1);
+            sessions.Add(session2);
+
+            return Json(new { Sessions = sessions }, JsonRequestBehavior.AllowGet);
+        }
     }
+
+    class Speaker
+    {
+        public string Name { get; set; }
+        public string About { get; set; }
+        public string Photo { get; set; }
+    }
+
+    class Session
+    {
+        public string Time { get; set; }
+        public string Title { get; set; }
+        public Speaker Speaker { get; set; }
+        public string About { get; set; }
+    }
+
 }
