0e9d0006362d47b66cc9d69dcec6a60181d7ac89
Konrad Kokosa <kkokosa@f28-studio.com>
2017-10-27 18:52:42 +0200

    2016 edition archived

---
+++ src/dotnetconfpl/dotnetconfpl/Areas/2016/Models/Speaker.cs
@@ -0,0 +1,10 @@
+﻿namespace dotnetconfpl.Areas._2016.Model
+{
+    public class Speaker
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string About { get; set; }
+        public string Photo { get; set; }
+    }
+}
---
+++ src/dotnetconfpl/dotnetconfpl/Areas/2016/_2016AreaRegistration.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+
+namespace dotnetconfpl.Areas._2016
+{
+    public class _2016AreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "2016";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "2016_default",
+                "2016/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional });
+        }
+    }
+}
