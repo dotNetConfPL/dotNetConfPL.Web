99dcf644471f0ae20eb75247af5a61abe24c98c8
Pawel Klimczyk <pawel@klimczyk.pl>
2017-12-14 10:03:26 +0100

    Added YT links for sessions

--- src/dotnetconfpl/dotnetconfpl/Model/DbContext.cs
+++ src/dotnetconfpl/dotnetconfpl/Model/DbContext.cs
@@ -80,7 +80,7 @@ namespace dotnetconfpl.Model
                 Title = "Co nowego w C#7, 7.x i....8!",
                 Speaker = marcin_zajkowski,
                 About = "Wielu programistów .NET nie przywykła (bądź nawet nie próbowała przywyknąć - #smuteczek) do stosowania wszystkich udogodnień, które udostępnione zostały nam w wersji 6 języka C#. Tymczasem na mapie wydanych wersji widnieje już wersja 7, 7.1 i 7.2, a na Githubie trwają burzliwe dyskusje na temat zakresu zmian w C# 8! Co nowego szykują nam twórcy języka? Czy powinniśmy obawiać się nadchodzących zmian i wyjścia poza strefę naszych programistycznych nawyków? Czy powinniśmy refaktoryzować nasze projekty, aby być zawsze na bieżąco w ramach platformy? Na te i inne pytania odpowiemy w trakcie tej prezentacji.",
-                YoutubeUrl = string.Empty
+                YoutubeUrl = "https://www.youtube.com/embed/bzrjECddq7Y"
             };
 
             var session2 = new Session
@@ -90,7 +90,7 @@ namespace dotnetconfpl.Model
                 Title = "CCC - Cloud, Continious, Configuration",
                 Speaker = piotr_stapp,
                 About = "CCC na pierwszy rzut oka kojarzy się z firmą obuwniczą, ale o niej w tej sesji nic nie będzie. Co innego chmura tu skojarzenia idą dużo dalej. W większości przypadków jest to marzenie dla deweloperów, architektów czy nawet PM. Cały zestaw usług 'As A Service' + serverless to cała masa pomysłów. Jednak zarządzanie i wdrażanie ich nie jest aż tak proste. Dla dużej ilości usług i aplikacji konfiguracja prosta nie jest. Szczególnie, że maszyny wirtualne są bleee i fuj, a usług do wyboru bez liku. W trakcie sesji pokażę jak to robimy w FinAi (młodym statrtup z kategorii FinTech), w prawdziwej chmurze (tu wystąpi Azure), z punktu widzenia deweloperów (bo administratorów za bardzo u nas nie ma), na naszym CI (w głównej roli Visual Studio Team Services).",
-                YoutubeUrl = string.Empty
+                YoutubeUrl = "https://www.youtube.com/embed/pqOVmAVyrYY"
             };
 
             var session3 = new Session
@@ -100,7 +100,7 @@ namespace dotnetconfpl.Model
                 Title = "Ile (nie) wiesz o strukturach w .NET",
                 Speaker = lukasz_pyrzyk,
                 About = "Kiedy ostatnio stworzyłeś nową strukturę pisząc aplikację w .NET? Wiesz do czego wykorzystywać struktury i jak mogą one zwiększyć wydajność Twojego programu? W prezentacji pokażę czym charakteryzują się struktury, jak dużo różni je od klas oraz opowiem o kilku ciekawych eksperymentach.",
-                YoutubeUrl = string.Empty
+                YoutubeUrl = "https://www.youtube.com/embed/JU_7dO-OMpc"
             };
 
             var session4 = new Session
@@ -110,7 +110,7 @@ namespace dotnetconfpl.Model
                 Title = "Grywalizacja + projektowanie na wybranym przykładzie.",
                 Speaker = krzysztof_owsiany,
                 About = "Grywalizacja – jak sama nazwa wskazuje wiąże się z grami komputerowymi. W czasie prezentacji przedstawię czym jest grywalizacja. Gdzie można ją użyć. Jakie korzyści niesie ze sobą ten ciekawy mechanizm wydarty prosto ze światka gier komputerowych. Zaprezentuję zestaw mechanizmów składających się na mechanikę gier, jakie można wykorzystać budując oprogramowanie komputerowe. Omówię kilka przykładów wykorzystania mechaniki gier w codziennym życiu. Na koniec pokuszę się o zaprojektowanie mechanizmu grywalizacji na prostym przykładzie.",
-                YoutubeUrl = string.Empty
+                YoutubeUrl = "https://www.youtube.com/embed/94JwR8Bnnuo"
             };
 
             var session5 = new Session
@@ -120,7 +120,7 @@ namespace dotnetconfpl.Model
                 Title = "Porównanie MVC w F# z Suave oraz w C# z ASP.NET Core",
                 Speaker = marian_dziubak,
                 About = "Odświeżymy sobie informację o wzorcu projektowym Model-View-Controller oraz przeanalizujemy aplikację napisaną w dwóch odmiennych środowiskach: w F# z biblioteką Suave oraz w C# z ASP.NET Core. Przejdziemy od konfiguracji środowiska, przez modele, widoki, kontrolery, aż do podłączenia do naszej aplikacji bazy danych. Po prezentacji będziecie posiadali wiedzę na temat tego jak napisać prostą aplikację Suave lub ASP.NET Core oraz jak niektóre konstrukcje języków C# i F# przyczyniają się do używania określonych wzorców programistycznych.",
-                YoutubeUrl = string.Empty
+                YoutubeUrl = "https://www.youtube.com/embed/fiSBbTjymek"
             };
 
             var session6 = new Session
@@ -130,7 +130,7 @@ namespace dotnetconfpl.Model
                 Title = "Ręczne zarządzanie pamięcią w .NET",
                 Speaker = adam_furmanek,
                 About = "Obiekty referencyjne na stercie, obiekty wartościowe na stosie — to typowa odpowiedź na pytanie o położenie w pamięci alokowanych instancji. Ale co dokładnie na temat struktury pamięci mówi standard .NET? Czy wymaga konkretnego zachowania w tym obszarze, czy może istnieją mechanizmy pozwalające nam przejąć kontrolę nad alokacją? Czy da się przenieść obiekt referencyjny na stos? A jeżeli tak, to co jeszcze można zrobić w języku, w którym tworzeniem i niszczeniem obiektów zajmuje się platforma? W trakcie prezentacji pokażę, jak ręcznie zaalokować obiekt w dowolnym miejscu i napisać własny operator new.Będzie trochę asemblera, podglądania zrzutów pamięci, słów kluczowych nierozpoznawanych przez ReSharpera, a wszystko to w C# — języku, w którym na pierwszy rzut oka nie da się tego zrobić.",
-                YoutubeUrl = string.Empty
+                YoutubeUrl = "https://www.youtube.com/embed/Hxim7HXKSro"
             };
 
             var session7 = new Session
@@ -140,7 +140,7 @@ namespace dotnetconfpl.Model
                 Title = "Xamarin - jak się do tego zabrać?",
                 Speaker = damian_antonowicz,
                 About = "Zastanawiałeś się nad tym jak tworzyć efektywniej natywne mobilne aplikacje cross-platform? Podczas tej sesji dowiesz się jak może Ci w tym pomóc znajomość Xamarina, którego popularność stale rośnie. Dowiesz się czym jest Xamarin i jak tak naprawdę działa oraz kiedy warto go użyć. Podzielę się z Tobą nabytym doświadczeniem, najlepszymi praktykami oraz wskażę kierunek, w którym możesz dalej się rozwijać.",
-                YoutubeUrl = string.Empty
+                YoutubeUrl = "https://www.youtube.com/embed/LplMe9TvlsM"
             };
 
             var session8 = new Session
@@ -150,7 +150,7 @@ namespace dotnetconfpl.Model
                 Title = "It's all about the state, czyli co skrywa async/await w C#?",
                 Speaker = dariusz_pawlukiewicz,
                 About = "Przez ostatnie lata programowanie asynchroniczne mocno się spopularyzowało, a jego użycie np. przy wykonywaniu żądań HTTP nikogo już nie dziwi. Język C# umożliwia nam bardzo prostą i przyjemną implementację kodu asynchronicznego przy wykorzystaniu async/await. Czy aby na pewno? Na sesji uczestnicy zostaną zapoznani z mechaniką async/await, aby następnie zanurkować prosto do świata kodu pośredniego (Intermediate Language) gdzie dowiedzą się jak ciężko musi napracować się kompilator, aby życie programisty było łatwe i przyjemne.",
-                YoutubeUrl = string.Empty
+                YoutubeUrl = "https://www.youtube.com/embed/paT7wWfH1wA"
             };
 
             _speakers.Add(marcin_zajkowski);
