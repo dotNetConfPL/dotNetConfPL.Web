44b2b93abad19dfd93ebb6c18788f5faa739de1d
Paweł Łukasik <lukasik.pawel+github@gmail.com>
2019-09-10 20:19:52 +0200

    Added session descriptions

--- src/dotnetconfpl/dotnetconfpl/Model/DbContext.cs
+++ src/dotnetconfpl/dotnetconfpl/Model/DbContext.cs
@@ -91,12 +91,23 @@ namespace dotnetconfpl.Model
             Speakers.Add(pawelHaracz);
 
 
-            var session1 = new Session {Speaker = piotrAndDarek, Title= "Convey - prosty przepis na mikroserwisy" };
-            var session2 = new Session {Speaker = joannaLamch, Title = "Internet of Things on Azure" };
-            var session3 = new Session {Speaker = piotrStapp, Title = "Orkiestracja to zło (konieczne)" };
-            var session4 = new Session {Speaker = kamilGrzybek, Title = "From Anemic to Rich Domain Model: Refactoring live session." };
-            var session5 = new Session {Speaker = januszNowak, Title = "Use Azure DevOps to depoly Azure Function in Docker on Raspberry Pi." };
-            var session6 = new Session {Speaker = pawelHaracz, Title = "Jak Event-driven Development pozwolił zbudować skalowalny system" };
+            var session1 = new Session {Speaker = piotrAndDarek, Title= "Convey - prosty przepis na mikroserwisy", Time = "Piątek (4.10) 18:00 - 19:00", About = @"Systemy rozproszone od ostatnich lat znów zyskują na popularności, a wszechobecne narzędzia związane z szeroko pojętą infrastrukturą, wdrożeniem czy orkiestracją usług pojawiają się niczym grzyby po deszczu. Z czym jednak musi się zmierzyć programista, aby stworzyć swój własny mikroserwis, a następnie zintegrować go z pozostałymi aplikacjami? Czy ekosystem platformy .NET Core jest wystarczająco przyjazny i bogaty w biblioteki wspomagające rozwój takiej architektury? Na te oraz inne pytania poszukamy odpowiedzi w trakcie prezentacji." };
+            var session2 = new Session {Speaker = joannaLamch, Title = "Internet of Things on Azure", Time = "Piątek (4.10) 19:00 - 20:00", About = @"W roku 2020 będzie 4 razy więcej podłączonych urządzeń niż ludzi na świecie.
+                To ogromny rynek dla nowych projektów i nowych umiejętności.Azure proponuje kilka gotowych komponentów oraz pokazuje jak takie projekty realizować.Opowiem o architekturze referencyjnej i klockach do jej zbudowania. Rzucę trochę światła na IoT w oparciu o chmurę Microsoft."};
+            var session3 = new Session {Speaker = piotrStapp, Title = "Orkiestracja to zło (konieczne)", Time = "Piątek (4.10) 20:00 - 21:00", About = @"Mała lekcja historii:
+
+- 20+ lat temu instalowaliśmy gry z dyskietek
+- 15 lat temu instalacje odbywały się za pomocą płyt CD
+- 10 lat temu nikt nie słyszał o mikro-serwisach
+- 6 lat temu docker nie istniał
+A teraz nagle orkiestracja to 'must - have' i nie da się pracować bez niej.
+
+                Na przykładzie Kubernetes(ludzie mówią, że to teraz 'TA' technologia) spojrzymy sobie na to czym jest orkiestracja,
+                jakie problemy ona rozwiązuje(o ile je mamy) i co możemy uzyskać."};
+            var session4 = new Session {Speaker = kamilGrzybek, Title = "From Anemic to Rich Domain Model: Refactoring live session.", Time = "Sobota (5.10) 11:00 - 12:00", About = @"Czy masz już dosyć utrzymywania i rozwijania kodu spaghetti? Czy wielka kula błota tocząca się po Twoim repozytorium kodu uniemożliwia Ci dostarczanie wartości biznesowej? Czy zmiana w jednej klasie wywołuje lawinę zmian niczym strzał z shotgun'a? Jeżeli przynajmniej na jedno pytanie odpowiedziałeś twierdząco to czas włożyć 'Refactoring Hat' i zaprowadzić należyty porządek w kodzie. Na sesji programowania 'na żywo' zostaną zaprezentowane techniki jak zmienić Model Domenowy z anemicznego na czysty i bogaty w zachowanie. Refaktoryzacja pełną gębą - Martin Fowler będzie dumny." };
+            var session5 = new Session {Speaker = januszNowak, Title = "Use Azure DevOps to depoly Azure Function in Docker on Raspberry Pi.", Time = "Sobota (5.10) 12:00 - 13:00", About = @"Using Azure DevOps Pipelines for creating and running Azure Functions in Container on Raspberry Pi.
+Azure DevOps Pipelines how to create containers and making deploy of container for running Azure Functions on Raspberry Pi." };
+            var session6 = new Session {Speaker = pawelHaracz, Title = "Jak Event-driven Development pozwolił zbudować skalowalny system", Time = "Sobota (5.10) 13:00 - 14:00", About = @"W prezentacji zostanie przedstawiona droga jak zbudowana platforma do obsługi urządzeń iot przestała być wydajna oraz co musiało zostać zmienione aby system był gotowy na obsłużenie kilku tysięcy urządzeń." };
 
             Sessions.Add(session1);
             Sessions.Add(session2);
